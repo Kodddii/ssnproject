81cb717a02cbc889d824f4e6a1f957fd4a2b0ff0
Kodddii <justic521@gmail.com>
2022-04-11 16:56:44 +0900

    follow friendId, totalTIme 기능 추가

--- router/checkin.js
+++ router/checkin.js
@@ -12,19 +12,21 @@ moment.tz.setDefault("Asia/seoul")
 router.get('/searchfollow',(req,res)=>{
     const {userId} = req.body;
     const token = req.headers.cookie.split('=')[1];
-    
-    
-    
-
+    const decoded = 1
 });
 
 
 
 
 
-router.post('/searchFollow',(req,res)=>{
-    const {userId} = req.body;
-    const token = req.headers.cookie.split('=')[1];
+router.post('/searchFollow',async(req,res)=>{
+    const {userId,friendId} = req.body;
+    const existUser = await User.find({friendId});
+    if(!existUser){
+        return res.send({msg:"없는 유저입니다!"})
+    }
+    await User.updateOne({userId},{$addToSet:{friendList:`${friendId}`}})
+    return res.send({msg:"추가완료"})
 });
 
 
@@ -35,7 +37,7 @@ router.post('/addinfo', async(req,res)=>{
 
 
     const {userId,nickname,password,startTime,totalTime,connecting,friendList} = req.body
-    console.log({userId,nickname,password,startTime,totalTime,connecting,friendList})
+
     await User.create({userId,nickname,password,startTime,totalTime,connecting,friendList})
     return res.status(200).send({msg:"완료!"});
 })
@@ -54,18 +56,23 @@ router.post('/:userId/start',async(req,res)=>{
 
 router.post('/:userId/end', async(req,res)=>{
     const {userId} = req.body;
-    const startTime = await User.find({userId}).startTime;
-    const endTime = new moment();
-    const todayTime = moment.duration(endTime.diff(startTime)).asHours()
     const userInfo = await User.find({userId})
-    // accum type : 누적시간 분으로 환산한 number 
-    const totalTime = userInfo.totalTime + todayTime
-    // accum활용
-    // let hours = parseInt(accum/60);
-    // let minutes = accum % 60
-    // let accumTime = `${hours}시간 ${minutes}분`
+    // db에 있는 startTime 꺼내오기
+    const startTimeIndb = userInfo[0].startTime
+    // string타입인 startTime moment object로 바꿔주기
+    const startTime = moment(startTimeIndb,'YYYY-MM-DD HH:mm');
+    // endTime 생성
+    const endTime = new moment();
+    // 시간차 계산
+    const todayTime = moment.duration(endTime.diff(startTime)).asMinutes()
+   
     
-    await User.updateOne({userId},{$set:{totalTime:totalTime}});
+     // type : 누적시간 분으로 환산한 number
+     // 총시간 계산
+    const totalTime = userInfo[0].totalTime + todayTime
+    // totalTime 데이터 추가, connecting false
+    await User.updateOne({userId},{$set:{totalTime:totalTime, connecting:false}});
+    return res.send({msg:"체크아웃완료!"})
     
 })
 
